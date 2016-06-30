0b74b6ae2c2a189759a2831f76b4af3a9c58ac09
Jakub Dudarewicz <jakub.dudarewicz@gmail.com>
2016-06-30 23:26:22 +0200

    koniec ćwiczeń

---
+++ workspace/cw58.m
@@ -0,0 +1,20 @@
+%XCORR
+
+s=sin([0:0.01:4*pi]);
+ss=s+rand(1,length(s))*0.2-0.1;
+
+[c, lags] = xcorr(s);
+[cc, lags1] = xcorr(s,ss);
+
+subplot(221)
+plot(s)
+title('sygnal wejsciowy')
+subplot(222)
+plot(ss)
+title('sygnal zaszumiony')
+subplot(223)
+plot(c)
+title('autokorelacja sygnalu zaszumionego')
+subplot(224)
+plot(c,cc)
+title('korelacja sygnalow')
